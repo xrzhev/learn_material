cb6c77f849ed112efbf9768313f5c02684797f2c
xrzhev <hypercore@outlook.jp>
2021-12-17 15:42:39 +0900

    add: 完成した

---
+++ rope_and_cutter/README.md
@@ -0,0 +1,1 @@
+`gcc -fno-stack-protector -no-pie -o rope_and_cutter ./rope_and_cutter.c`
--- rope_and_cutter/rope_and_cutter.c
+++ rope_and_cutter/rope_and_cutter.c
@@ -1,8 +1,10 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 
-char args[32] = "/bin/cat ./flag.txt";
+//char args[32] = "/bin/cat ./flag.txt";
+char args[32] = "/bin/bash";
 
 void initialize(void) {
     alarm(60);
@@ -11,9 +13,12 @@ void initialize(void) {
     setvbuf(stderr, NULL, _IONBF, 0);
 }
 
+void __unused(void) {
+    __asm__("pop %rdi;ret;");
+}
+
 int main(void) {
     char buf[32];
-
     initialize();
     gets(buf, stdin);
     system("/bin/echo 'Hello, world!'");
